369e5bf3a811504ee9bdd5877b8841adf32af52b
paded <paded.ku.57@ubu.ac.th>
2018-01-16 16:45:58 +0700

    add color

--- app/src/main/java/ubontransitdriver/paded/com/ubontransitdriver/CreateAccountActivity.java
+++ app/src/main/java/ubontransitdriver/paded/com/ubontransitdriver/CreateAccountActivity.java
@@ -1,46 +1,115 @@
 package ubontransitdriver.paded.com.ubontransitdriver;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.h6ah4i.android.materialshadowninepatch.MaterialShadowContainerView;
 
 public class CreateAccountActivity extends AppCompatActivity {
-    private EditText inputEmail, inputPassword;
-    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private EditText inputEmail, inputPassword, inputName;
+    private Button btnCreateAccount, btnLogin, btnResetPassword;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
+    private String TAG = "TEST22";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account_layout);
 
-        MaterialShadowContainerView shadowView =
-                (MaterialShadowContainerView) findViewById(R.id.shadow_item_container);
-
-        float density = getResources().getDisplayMetrics().density;
-
-        shadowView.setShadowTranslationZ(density * 2.0f); // 2.0 dp
-        shadowView.setShadowElevation(density * 4.0f); // 4.0 dp
-
-
         //Get Firebase auth instance
-//        auth = FirebaseAuth.getInstance();
-//
-//        btnSignIn = (Button) findViewById(R.id.sign_in_button);
-//        btnSignUp = (Button) findViewById(R.id.sign_up_button);
-//        inputEmail = (EditText) findViewById(R.id.email);
-//        inputPassword = (EditText) findViewById(R.id.password);
-//        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-//        btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
+        auth = FirebaseAuth.getInstance();
+
+        inputEmail = (EditText)findViewById(R.id.input_email);
+        inputPassword = (EditText)findViewById(R.id.input_password);
+        inputName = (EditText)findViewById(R.id.input_name);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        btnCreateAccount = (Button)findViewById(R.id.btn_create_account);
+        btnResetPassword = (Button)findViewById(R.id.btn_reset_password);
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+                final String userName = inputName.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
 
 
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
+                if (TextUtils.isEmpty(userName)) {
+                    Toast.makeText(getApplicationContext(), "Enter name!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
+                progressBar.setVisibility(View.VISIBLE);
+
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(CreateAccountActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            FirebaseUser user = auth.getCurrentUser();
+                            Log.d(TAG, "onComplete: "+user.getUid());
+                            insertData(user.getUid(),userName);
+                            startActivity(new Intent(CreateAccountActivity.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    public void insertData(String uid, String name){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users/"+uid);
+
+        myRef.child("name").setValue(name);
+        myRef.child("bus_line").setValue(name);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
     }
 }
