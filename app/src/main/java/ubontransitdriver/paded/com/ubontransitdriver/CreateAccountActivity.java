8f50c75ebbeb12efc1e217414cf2f6e705368bc6
paded <paded.ku.57@ubu.ac.th>
2018-01-19 16:01:26 +0700

    read bus data from firebase

--- app/src/main/java/ubontransitdriver/paded/com/ubontransitdriver/CreateAccountActivity.java
+++ app/src/main/java/ubontransitdriver/paded/com/ubontransitdriver/CreateAccountActivity.java
@@ -5,14 +5,19 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -23,20 +28,29 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.h6ah4i.android.materialshadowninepatch.MaterialShadowContainerView;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class CreateAccountActivity extends AppCompatActivity implements ItemAdapter.ItemListener{
     private EditText inputEmail, inputPassword, inputName;
     private Button btnCreateAccount, btnLogin, btnResetPassword, btnAddBus;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
-    private ScrollView mainLayout;
     private String TAG = "TEST22";
-
+    private FrameLayout layout_MainMenu;
     BottomSheetDialog bottomSheetDialog;
     BottomSheetBehavior bottomSheetBehavior;
+
+    RecyclerView recyclerView;
+    private ItemAdapter mAdapter;
+    CoordinatorLayout coordinatorLayout;
+    BottomSheetBehavior behavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +60,7 @@ public class CreateAccountActivity extends AppCompatActivity {
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
 
-        mainLayout = (ScrollView) findViewById(R.id.mainlayout);
+//        mainLayout = (ScrollView) findViewById(R.id.mainlayout);
 
         inputEmail = (EditText)findViewById(R.id.input_email);
         inputPassword = (EditText)findViewById(R.id.input_password);
@@ -59,31 +73,34 @@ public class CreateAccountActivity extends AppCompatActivity {
         btnLogin = (Button)findViewById(R.id.btnLogin);
         btnAddBus = (Button)findViewById(R.id.btn_addBus);
 
-        View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet_layout, null);
-        bottomSheetDialog = new BottomSheetDialog(CreateAccountActivity.this);
-        bottomSheetDialog.setContentView(bottomSheetView);
+        layout_MainMenu = (FrameLayout) findViewById( R.id.framelayout);
+        layout_MainMenu.getForeground().setAlpha( 0);
 
-        bottomSheetBehavior = BottomSheetBehavior.from((View) bottomSheetView.getParent());
+//        View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet_layout, null);
+//        bottomSheetDialog = new BottomSheetDialog(CreateAccountActivity.this);
+//        bottomSheetDialog.setContentView(bottomSheetView);
+//
+//        bottomSheetBehavior = BottomSheetBehavior.from((View) bottomSheetView.getParent());
+//
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+//        TextView addMenu = (TextView) bottomSheetView.findViewById(R.id.menu_add_bus0);
+//        TextView editMenu = (TextView) bottomSheetView.findViewById(R.id.menu_add_bus1);
+//        TextView deleteMenu = (TextView) bottomSheetView.findViewById(R.id.menu_add_bus2);
 
-        TextView addMenu = (TextView) bottomSheetView.findViewById(R.id.menu_add_bus0);
-        TextView editMenu = (TextView) bottomSheetView.findViewById(R.id.menu_add_bus1);
-        TextView deleteMenu = (TextView) bottomSheetView.findViewById(R.id.menu_add_bus2);
-
-        addMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                bottomSheetDialog.hide();
-            }
-        });
+//        addMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                bottomSheetDialog.hide();
+//            }
+//        });
 
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(mainLayout.getWindowToken(), 0);
+                imm.hideSoftInputFromWindow(coordinatorLayout.getWindowToken(), 0);
 
                 String email = inputEmail.getText().toString().trim();
                 String password = inputPassword.getText().toString().trim();
@@ -152,10 +169,76 @@ public class CreateAccountActivity extends AppCompatActivity {
             }
         });
 
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+
+
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        behavior = BottomSheetBehavior.from(bottomSheet);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+                Log.d(TAG, "onStateChanged: "+newState);
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                if(slideOffset == 0.0){
+                    layout_MainMenu.getForeground().setAlpha(0);
+                }
+                Log.d(TAG, "slideOffset: "+slideOffset);
+                // React to dragging events
+            }
+        });
+
+
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Get a reference to our posts
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("server/saving-data/fireblog/posts");
+
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Post post = dataSnapshot.getValue(Post.class);
+                System.out.println(post);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+
+
+
+        ArrayList<String> items = new ArrayList<>();
+        items.add("Item 16");
+        items.add("Item 2");
+        items.add("Item 3");
+        items.add("Item 4");
+        items.add("Item 5");
+        items.add("Item 6");
+
+        mAdapter = new ItemAdapter(items, this);
+        recyclerView.setAdapter(mAdapter);
+
         btnAddBus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bottomSheetDialog.show();
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(coordinatorLayout.getWindowToken(), 0);
+                layout_MainMenu.getForeground().setAlpha(100);
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
             }
         });
 
@@ -173,5 +256,28 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (behavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+            behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            layout_MainMenu.getForeground().setAlpha(0);
+            Log.d(TAG, "onBackPressed:1 "+behavior.getState());
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
+//    for button sheet click
+    @Override
+    public void onItemClick(String item) {
+
+        Snackbar.make(coordinatorLayout,item + " is selected", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+        behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
     }
 }
